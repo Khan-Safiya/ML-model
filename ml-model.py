97f0d26e5c6b33a9ea8e773b39b8a2e288581967
Khan-Safiya <khansafiyakhan250705@gmail.com>
2026-04-23 10:05:36 +0530

    ML Model V1 added

--- ml-model.py
+++ ml-model.py
@@ -13,7 +13,7 @@ df['Admitted']=np.where(df['Chance']>=0.5,1,0)
 X=df[["GRE Score","CGPA"]]
 y=df["Admitted"]
 model=DecisionTreeClassifier()
-X_train, X_test, y_train, y_test= train_test_split(X,y,test_size=0.3,random_state=64)
+X_train, X_test, y_train, y_test= train_test_split(X,y,test_size=0.2,random_state=42)
 model.fit(X_train,y_train)
 y_pred=model.predict(X_test)
 
@@ -25,11 +25,3 @@ print("\nClassification Report:\n", classification_report(y_pred,y_test))
 
 print("Actual Values: \n", y_test[:10].values)
 print("Predicted Values: \n", y_pred[:10])
-
-gre=float(input("\nEnter the GRE Score (out of 340): "))
-cgpa=float(input("Enter the CGPA (out of 10): "))
-y_pred=model.predict([[gre,cgpa]])
-if y_pred[0]==1:
-    print("\nThe student is likely to get admission.")
-else:
-    print("\nThe student is unlikely to get admission.")
